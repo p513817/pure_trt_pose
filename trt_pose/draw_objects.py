a3f30b99392d697762c058bfbccbb870f3679b73
MaxChangInnodisk <max_chang@innodisk.com>
2022-02-17 13:44:40 +0800

    feat: add random color and return draw image.

--- trt_pose/draw_objects.py
+++ trt_pose/draw_objects.py
@@ -1,30 +1,27 @@
-import cv2
-
+import cv2, random
 
 class DrawObjects(object):
     
-    def __init__(self, topology):
+    def __init__(self, topology, color=None, alpha=0.5):
         self.topology = topology
-        
+        self.colors=[ (random.randint(0,255), random.randint(0,255), random.randint(0,255)) for _ in range((topology.shape[0]+1)) ] if color==None else color
+        self.alpha = alpha		
+
     def __call__(self, image, object_counts, objects, normalized_peaks):
         topology = self.topology
         height = image.shape[0]
         width = image.shape[1]
-        
+
+        image_org, image_draw = image.copy(), image.copy()
+
         K = topology.shape[0]
         count = int(object_counts[0])
-        K = topology.shape[0]
+        
         for i in range(count):
-            color = (0, 255, 0)
+            
             obj = objects[0][i]
+            
             C = obj.shape[0]
-            for j in range(C):
-                k = int(obj[j])
-                if k >= 0:
-                    peak = normalized_peaks[0][j][k]
-                    x = round(float(peak[1]) * width)
-                    y = round(float(peak[0]) * height)
-                    cv2.circle(image, (x, y), 3, color, 2)
 
             for k in range(K):
                 c_a = topology[k][2]
@@ -36,4 +33,16 @@ class DrawObjects(object):
                     y0 = round(float(peak0[0]) * height)
                     x1 = round(float(peak1[1]) * width)
                     y1 = round(float(peak1[0]) * height)
-                    cv2.line(image, (x0, y0), (x1, y1), color, 2)
+                    cv2.line(image_draw, (x0, y0), (x1, y1), self.colors[k], 2)
+
+            for j in range(C):
+                k = int(obj[j])
+                if k >= 0:
+                    peak = normalized_peaks[0][j][k]
+                    x = round(float(peak[1]) * width)
+                    y = round(float(peak[0]) * height)
+                    cv2.circle(image_draw, (x, y), 3, self.colors[j], 2)
+
+        image_draw = cv2.addWeighted(image_org, self.alpha, image_draw, 1 - self.alpha, 0)
+
+        return image_draw
